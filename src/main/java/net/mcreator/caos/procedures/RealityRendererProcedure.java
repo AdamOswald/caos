6f79bf7b93e623efe7012264080b42accf0fb415
Adam Oswald <adamoswald69420@gmail.com>
2025-10-08 13:53:35 -0400

    Add files via upload

---
+++ src/main/java/net/mcreator/caos/procedures/RealityRendererProcedure.java
@@ -0,0 +1,78 @@
+package net.mcreator.caos.procedures;
+
+import net.mcreator.caos.enchantment.RealitySplitterEnchantment;
+import net.mcreator.caos.CaosModElements;
+import net.mcreator.caos.CaosMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@CaosModElements.ModElement.Tag
+public class RealityRendererProcedure extends CaosModElements.ModElement {
+	public RealityRendererProcedure(CaosModElements instance) {
+		super(instance, 2);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				CaosMod.LOGGER.warn("Failed to load dependency entity for procedure RealityRenderer!");
+			return;
+		}
+		if (dependencies.get("sourceentity") == null) {
+			if (!dependencies.containsKey("sourceentity"))
+				CaosMod.LOGGER.warn("Failed to load dependency sourceentity for procedure RealityRenderer!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		Entity sourceentity = (Entity) dependencies.get("sourceentity");
+		if ((ItemTags.getCollection().getTagByID(new ResourceLocation(("minecraft:sword").toLowerCase(java.util.Locale.ENGLISH))).contains(
+				((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY).getItem()))) {
+			if (((EnchantmentHelper.getEnchantmentLevel(RealitySplitterEnchantment.enchantment,
+					((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)) != 0))) {
+				(((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY))
+						.addEnchantment(RealitySplitterEnchantment.enchantment, (int) 1);
+				(((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)).setDamage(
+						(int) (((((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY))
+								.getDamage())
+								+ (6.75 * ((EnchantmentHelper.getEnchantmentLevel(RealitySplitterEnchantment.enchantment,
+										((sourceentity instanceof LivingEntity)
+												? ((LivingEntity) sourceentity).getHeldItemMainhand()
+												: ItemStack.EMPTY)))
+										+ 0.5))));
+			}
+		}
+		entity.attackEntityFrom(DamageSource.GENERIC,
+				(float) (((((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY))
+						.getDamage())
+						+ (6.75 * ((EnchantmentHelper.getEnchantmentLevel(RealitySplitterEnchantment.enchantment,
+								((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)))
+								+ 0.5))));
+	}
+
+	@SubscribeEvent
+	public void onEntityAttacked(LivingAttackEvent event) {
+		if (event != null && event.getEntity() != null) {
+			Entity entity = event.getEntity();
+			Entity sourceentity = event.getSource().getTrueSource();
+			Entity imediatesourceentity = event.getSource().getImmediateSource();
+			double i = entity.getPosX();
+			double j = entity.getPosY();
+			double k = entity.getPosZ();
+			double amount = event.getAmount();
+			World world = entity.world;
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("x", i);
+			dependencies.put("y", j);
+			dependencies.put("z", k);
+			dependencies.put("amount", amount);
+			dependencies.put("world", world);
+			dependencies.put("entity", entity);
+			dependencies.put("sourceentity", sourceentity);
+			dependencies.put("imediatesourceentity", imediatesourceentity);
+			dependencies.put("event", event);
+			this.executeProcedure(dependencies);
+		}
+	}
+}
