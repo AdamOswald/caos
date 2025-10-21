d425ec0416f7579358345ea58b4fc7406f892e04
Adam Oswald <adamoswald69420@gmail.com>
2025-10-21 12:04:17 -0400

    Update RealitySplitterEnchantment.java

--- src/main/java/net/mcreator/caos/enchantment/RealitySplitterEnchantment.java
+++ src/main/java/net/mcreator/caos/enchantment/RealitySplitterEnchantment.java
@@ -22,6 +22,27 @@ public class RealitySplitterEnchantment extends CaosModElements.ModElement {
 			super(Enchantment.Rarity.VERY_RARE, EnchantmentType.WEAPON, slots);
 		}
 
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraftforge.registries.ObjectHolder;
+import net.mcreator.caos.CaosModElements;
+@CaosModElements.ModElement.Tag
+public class RealitySplitterEnchantment extends CaosModElements.ModElement {
+    @ObjectHolder("caos:reality_splitter")
+    public static final Enchantment enchantment = null;
+    public RealitySplitterEnchantment(CaosModElements instance) {
+        super(instance, 1);
+    }
+    @Override
+    public void initElements() {
+        elements.enchantments.add(() -> new CustomEnchantment(EquipmentSlotType.MAINHAND).setRegistryName("reality_splitter"));
+    }
+    public static class CustomEnchantment extends Enchantment {
+        public CustomEnchantment(EquipmentSlotType... slots) {
+            super(Enchantment.Rarity.VERY_RARE, EnchantmentType.WEAPON, slots);
+        }
+
 		@Override
 		public int getMinLevel() {
 			return 1;
